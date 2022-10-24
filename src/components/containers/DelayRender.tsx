0bea21e499d661ff660c1a47180b248c116bd9b7
Matt Lim <mlimcollege@gmail.com>
2022-10-23 21:08:38 -0400

    Fix small issue with DelayRender

--- src/components/containers/DelayRender.tsx
+++ src/components/containers/DelayRender.tsx
@@ -4,12 +4,20 @@ type Props = {
   children: any;
 };
 
+// We only need this to happen once, and not every time
+// the children get re-rendered (e.g. if you nav from one page
+// to another)
+let isShownGlobal = false;
+
 // Delay render so that we know whether the wallet is
 // connected or not
 export default function DelayRender({ children }: Props) {
-  const [isShown, setIsShown] = useState(false);
+  const [isShown, setIsShown] = useState(isShownGlobal);
   useEffect(() => {
-    setTimeout(() => setIsShown(true), 100);
+    setTimeout(() => {
+      setIsShown(true);
+      isShownGlobal = true;
+    }, 100);
   }, []);
 
   if (!isShown) {
