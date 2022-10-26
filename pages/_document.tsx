39222318be25107c7f068f18226cfda0d7fbb63d
Matt Lim <mlimcollege@gmail.com>
2022-10-26 18:28:10 -0400

    Update header metadata

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,9 +5,9 @@ const META_DESCRIPTION =
   "Solana's best coin flip app. Double your SOL with just a single coin flip!";
 // Different dimensions than twitter
 const META_IMAGE_FACEBOOK =
-  "https://firebasestorage.googleapis.com/v0/b/minymon-d541f.appspot.com/o/link_previews%2Fminymon-facebook-preview.png?alt=media&token=e58539f0-897c-4584-8771-dce0140e4174";
+  "https://firebasestorage.googleapis.com/v0/b/hammyflip-c598c.appspot.com/o/hammyflip-facebook-preview.jpg?alt=media&token=b64eb2b6-19d7-4ded-8e2c-24d8c7ca7b09";
 const META_IMAGE_TWITTER =
-  "https://firebasestorage.googleapis.com/v0/b/minymon-d541f.appspot.com/o/link_previews%2Fminymon-twitter-preview.png?alt=media&token=eb49eab1-613f-4d5a-ae4f-43b81ef140e5";
+  "https://firebasestorage.googleapis.com/v0/b/hammyflip-c598c.appspot.com/o/hammyflip-twitter-preview.jpg?alt=media&token=fb442e98-c06b-4a20-9dc9-1d3976dfdff1";
 const META_TITLE = "Hammyflip | Flip a coin to double your SOL";
 
 export default function Document() {
@@ -27,20 +27,15 @@ export default function Document() {
         />
 
         {/* Facebook */}
-        <meta property="og:url" content="https://www.minymon.com/" />
+        <meta property="og:url" content="https://www.hammyflip.com/" />
         <meta property="og:type" content="website" />
         <meta property="og:title" content={META_TITLE} />
         <meta property="og:description" content={META_DESCRIPTION} />
         <meta property="og:image" content={META_IMAGE_FACEBOOK} />
-        <meta
-          name="facebook-domain-verification"
-          content="l75spkigtk8uytb844y4nfbkzaauib"
-        />
 
         {/* Twitter */}
         <meta name="twitter:card" content="summary_large_image" />
-        {/* TODO: add */}
-        {/* <meta name="twitter:site" content="@withwizards" /> */}
+        <meta name="twitter:site" content="@hammyflip" />
         <meta name="twitter:title" content={META_TITLE} />
         <meta name="twitter:description" content={META_DESCRIPTION} />
         <meta name="twitter:image" content={META_IMAGE_TWITTER} />
